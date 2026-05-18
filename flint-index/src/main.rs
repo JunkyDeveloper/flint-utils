ebfc1021f24b1d6373fef77732ac1346fd7a9a6f
JunkyDeveloper <junkydeveloper@gmail.com>
2026-05-18 22:10:22 +0200

    flint-index util

---
+++ flint-index/src/main.rs
@@ -0,0 +1,13 @@
+use dotenvy::dotenv;
+use flint_core::utils::get_test_path;
+use std::path::Path;
+
+fn main() {
+    dotenv().ok();
+    flint_core::loader::TestLoader::new(Path::new(&get_test_path()), true)
+        .ok()
+        .unwrap()
+        .verify_and_rebuild_index()
+        .expect("Verify wasn't successfull and rebuild also failed");
+    println!("Hello, world!");
+}
